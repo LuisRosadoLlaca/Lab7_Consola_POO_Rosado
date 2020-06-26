3a8b0a6d3d0f8ea0c2145250badef408806a1e0a
LuisRosado <luirosadol@upt.pe>
2020-06-25 20:14:22 -0500

    Version 1

---
+++ Notas.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7_Consola_POO_Rosado
+{
+    class Notas
+    {
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+
+        public double notaA { get; set; }
+        public double notaP { get; set; }
+        public double notaC { get; set; }
+
+        public double promedio { get; set; }
+        public string condicion { get; set; }//condicion del estudiante
+
+
+
+        //public Notas(string nombre, string apellido, double notaA, double notaP, double notaC)
+        //{
+        //    this.nombre = nombre;
+        //    this.apellido = apellido;
+        //    this.notaA = notaA;
+        //    this.notaP = notaP;
+        //    this.notaC = notaP;
+        //}
+
+        List<Notas> lista = new List<Notas>();
+
+        public void Agregar(Notas objNota)
+        {
+            lista.Add(objNota);
+        }
+
+
+        public void CalcularPromedio(double notaA, double notaP, double notaC)
+        {
+            this.promedio = (notaA * 0.10) + (notaP * 0.35) + (notaC * 0.55);
+            Console.WriteLine("Promedio : " + this.promedio);
+        }
+        public string ObjtenerCondicion()
+        {
+            if (this.promedio>=0 && this.promedio < 10.5)
+            {
+                condicion = "DESAPROBADO";
+            }
+            else if (this.promedio>=10.5 && this.promedio<=20)
+            {
+                condicion = "APROBADO";
+            }
+            else
+            {
+                condicion = "FUERA DE RANGO (0-20)";
+            }
+            return condicion;
+        }
+
+        public List<Notas> Listar()
+        {
+            return lista;
+        }
+    }
+}
---
+++ ProgramarNotas.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7_Consola_POO_Rosado
+{
+    class ProgramarNotas
+    {
+        static void Main(string[] args)
+        {
+            Ejecutar();
+        }
+
+        private static void Ejecutar()
+        {
+            Notas objLista = new Notas();
+
+
+            char opcion = ' ';
+            int numero = 1;
+            Console.WriteLine("==========================");
+            Console.WriteLine(" Notas de los Estudiantes ");
+            Console.WriteLine("==========================");
+            do
+            {
+                Notas objNota = new Notas();
+
+                Console.WriteLine("Agrengando estudiante: " + numero++);
+                Console.WriteLine("Ingrese Nombres: ");
+                objNota.nombre = Console.ReadLine();
+                Console.WriteLine("Ingrese Apellidos: ");
+                objNota.apellido = Console.ReadLine();
+                Console.WriteLine("Ingrese Nota Actitudina: ");
+                objNota.notaA = double.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese Nota Procedimental: ");
+                objNota.notaP = double.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese Nota Conceptual: ");
+                objNota.notaC = double.Parse(Console.ReadLine());
+
+                objLista.Agregar(objNota);
+                Console.WriteLine();
+
+                Console.WriteLine("Desea seguir agregando otro estudiante... (S) o (N)");
+                opcion = char.Parse(Console.ReadLine());
+
+            } while (opcion.Equals('s') || opcion.Equals('S'));
+
+            numero = 1;
+            Console.WriteLine("==========================");
+            Console.WriteLine(" Registro de Estudiantes ");
+            Console.WriteLine("==========================");
+
+            foreach (Notas e in objLista.Listar())
+            {
+                Console.WriteLine("N° : " + numero++);
+                Console.WriteLine("Nombres : " + e.nombre);
+                Console.WriteLine("Apellidos : " + e.apellido);
+                Console.WriteLine("Nota Actitudinal : "+ e.notaA);
+                Console.WriteLine("Nota Procedimental : " + e.notaP);
+                Console.WriteLine("Nota Conceptual : " + e.notaC);
+                objLista.CalcularPromedio(e.notaA, e.notaP, e.notaC);
+                objLista.ObjtenerCondicion();
+            }
+            Console.ReadKey();
+        }     
+    }
+}
